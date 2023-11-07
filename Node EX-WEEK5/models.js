212028848fb424e6780daa932531828337491577
SepehrYahyaee <7heycallmegray@gmail.com>
2023-11-07 21:16:04 +0330

    question1 Solved

--- Node EX-WEEK5/models.js
+++ Node EX-WEEK5/models.js
@@ -1,42 +1,41 @@
-class User{
-    constructor(userName){
-        this.userName = userName;
-        this.purchaseHistory = [];
-    }
+// Product, User, Comment
 
-    buy(){
-        // a "user" buys something from "products".
-    }
-}
-
-
+// درست و حسابی نتونستم فعلا بنویسمش ولی خب خواسته های سوال و یجوری برآورده کردم
 
 class Product{
-    static allIds = [];
-
-    static generateId(){
-        const generatedId = parseInt(Math.random() * 1e3);
-        while ((Product.allIds).includes(generatedId) && generatedId > 0){
-            generatedId = parseInt(Math.random() * 1e3);
-        }
-        (Product.allIds).push(generatedId);
-        return generatedId;
-    }
 
     constructor(name, price, category){
         this.name = name;
         this.price = price;
         this.category = category;
         this.id = Product.generateId();
-        this.comment = [];
+        this.commentSection = [];
+        this.ratings = [];
     }
 
-    addComment(comment){
-        (this.comment).push(comment);
+    static allIds = [];
+
+    static generateId(){
+        let generatedId = parseInt(Math.random() * 1e3);
+        while (Product.allIds.includes(generatedId)){
+            generatedId = parseInt(Math.random() * 1e3);
+        };
+        Product.allIds.push(generatedId);
+        return generatedId;
+    }
+}
+
+class User{
+
+    constructor(userName){
+        this.userName = userName;
+        this.purchaseHistory = []; // array of product ID's 
+        this.comments = [];
     }
 }
 
 class Comment{
+
     constructor(user, rating, text){
         this.user = user;
         this.rating = rating;
@@ -44,18 +43,8 @@ class Comment{
     }
 }
 
-// const user1 = new User("Ghazanfar");
-// const user2 = new User("Heshmat");
-
-
-// const product1 = new Product("چنگال", "تومن 100", "لوازم آشپزخانه");
-// const product2 = new Product("قاشق", "تومن 50", "لوازم آشپزخانه");
-// const product3 = new Product("موس", "2 میلیون", "لوازم جانبی کامپیوتر");
-
-// const comment1 = new Comment("Heshmat", 0, "با موس اش حال نکردم خیلی داغون بود");
-
 module.exports = {
-    User,
     Product,
-    Comment
+    User,
+    Comment,
 }
--- Node EX-WEEK5/store.js
+++ Node EX-WEEK5/store.js
@@ -1,35 +1,107 @@
-const { User, Product, Comment } = require("./models.js");
+const { Product, User, Comment} = require('./models.js');
 
-let productsById = {};
-let users = [];
-let allCount = 0;
+const products = {};
+const users = [];
 
 function addProduct(product, count){
-    productsById[product.id] = {product, count};
-    allCount += count;
+    products[product.id] = {product, count};
 }
 
-function removeProduct(product, count){
-    // !!
+function removeProduct(productId, count){
+    if (!Product.allIds.includes(productId)){
+        throw new Error("the product you're trying to remove doesn't exist!")
+    } else {
+        if (products[productId].count - count <= 0){
+            throw new Error('cannot have negative or no amount of products!!!!')
+        }
+        products[productId].count -= count;
+    }
 }
 
 function addUser(user){
-    users.push(user.userName)
+    users.push(user.userName);
 }
 
 function getTotalInventoryCount(){
-    return allCount;
+    let total = 0;
+    for (let ID of Product.allIds){
+        total += products[ID].count;
+    }
+    return total;
 }
 
-addProduct(new Product("Changal", "70 Toman", "Ashpazkhune"), 5);
-addProduct(new Product("Ghashogh", "70 Toman", "Ashpazkhune"), 15);
-addProduct(new Product("Boshghab", "150 Toman", "Ashpazkhune"), 20);
-addProduct(new Product("Ghablame", "200 Toman", "Ashpazkhune"), 5);
-addProduct(new Product("Chaghoo", "70 Toman", "Ashpazkhune"), 20);
-addProduct(new Product("Malaghe", "100 Toman", "Ashpazkhune"), 30);
-addProduct(new Product("Kafgir", "100 Toman", "Ashpazkhune"), 25);
+function getTotalProfit(){
+    let total = 0;
+    for (let ID of Product.allIds){
+        total += products[ID].product.price;
+    }
+    return total;
+}
 
-console.log(productsById);
-addUser(new User("Heshmat"));
-console.log(users);
-console.log(getTotalInventoryCount());
+function userBuy(user, productId, count){
+    if (!(users.includes(user.userName))){
+        throw new Error("this user doesn't exist!");
+    } else if (!(Product.allIds).includes(productId)){
+        throw new Error("this product doesn't exist!");
+    } else {
+        (user.purchaseHistory).push(products[productId].product.name);
+        products[productId].count -= count;
+    }
+}
+
+function addComment(comment, product){ // instance of the Comment class.
+    if (!(users.includes(comment.user.userName))){
+        throw new Error("this user doesn't exist!");
+    }
+    if (!(Product.allIds.includes(product.id))){
+        throw new Error("this product doesn't exist!")
+    }
+    product.commentSection.push(`${comment.text} - ${comment.user.userName}.`);
+    comment.user.comments.push(comment.text);
+    (product.ratings).push(comment.rating);
+}
+
+function getCommentsByUser(user){
+    if (!(users.includes(user.userName))){
+        throw new Error("User doesn't exist!");
+    }
+    return [user.comments];
+}
+
+function getRating(product){
+    let total = 0;
+    if (!(Product.allIds).includes(product.id)){
+        throw new Error("this product doesn't exist!");
+    }
+    for (let i = 0 ; i < product.ratings.length ; i++){
+        total += Number(product.ratings[i]);
+    }
+    return `${((total) / (product.ratings.length))} stars.`;
+}
+
+const product1 = new Product("Boshghab", 100, "Ashpazkhune");
+
+addProduct(product1, 10);
+// console.log(products);
+
+const user1 = new User('Heshmat')
+addUser(user1);
+// console.log(users);
+
+// console.log(getTotalInventoryCount());
+// console.log(getTotalProfit());
+
+userBuy(user1, product1.id, 2);
+
+// console.log(user1.purchaseHistory);
+// console.log(products);
+
+addComment(new Comment(user1, 2, "Boshghabe be dard nakhori bood"), product1);
+addComment(new Comment(user1, 5, "halalet ajab Boshqabie"), product1);
+
+// console.log(product1.commentSection);
+
+// console.log(getCommentsByUser(user1));
+// console.log(product1.ratings);
+// console.log(getRating(product1));
+// console.log(products);
